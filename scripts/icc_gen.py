2ff09fe4c9baaf369be5262790a4c8a03c7ed8da
apsuedonym <j.m.walker@live.co.uk>
2019-01-21 16:45:38 +0000

    Nearly finished scripts. Need to finish pbs

---
+++ scripts/icc_gen.py
@@ -0,0 +1,59 @@
+#!/lustre/sw/anaconda/anaconda3-5.1.0/bin/python3
+
+"""  icc-gen.py
+This script compiles a benchmarks, with -O3. After
+compiling each benchmark, they are ran in parallel
+to save time. Output is stored in a CSV
+"""
+
+from multiprocessing import Pool as ThreadPool
+from os import chdir, listdir, system
+from os.path import isfile
+
+def run_bench(cmd):
+    system('chmod +x run.sh')
+    exec_string = '(time ./run.sh) &> ' + arg + '.dat'
+    return cmd
+
+def edit_make(f):
+    lines = f.readlines()
+    f.seek(0)
+    f.truncate()
+    for line in lines:
+        if "gcc" in line and "#" not in line:
+            f.write('CC=icc')
+        else:
+            f.write(line)
+
+def main():
+
+    chdir('../coptbenchmarks2013')
+    my_array = list(range(1,11))
+    for folder in listdir():
+        if ( not isfile(folder)):
+            print('Entering ', folder)
+            chdir(folder + "/src")
+            with open('Makefile', 'r+') as Makefile:
+                edit_make(Makefile)
+
+            print('Compiling...')
+            system('make')
+            chdir('..')
+
+            # Now we must run the benchmark, in parallel to save time
+
+            pool = ThreadPool(processes=10)
+
+            results = pool.map(run_bench, my_array)
+            pool.close()
+            pool.join()
+
+            print('Completed execution for ', folder)
+            print(results)
+            results = []
+
+
+    print('Done')
+
+
+main()
